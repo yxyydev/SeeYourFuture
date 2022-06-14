679d13eb3135a134f8e40f3384a7b4a6ec3acf56
yxyydev <107167330+yxyydev@users.noreply.github.com>
2022-06-13 22:23:00 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const elementoResposta = document.querySelector('#resposta')
+const inputPergunta = document.querySelector('#inputPergunta')
+const respostas = [
+  'Yass Kween!',
+  'Não tenho tanta certeza.',
+  'É decididamente assim.',
+  'Não conte com isso.',
+  'Sem dúvidas!',
+  'Pergunte novamente mais tarde.',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melhor não te dizer agora.',
+  'A meu ver, sim.',
+  'Minhas fontes dizem não.',
+  'Provavelmente.',
+  'Não é possível prever agora.',
+  'Perspectiva boa.',
+  'As perspectivas não são tão boas.',
+  'Sim.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim.'
+]
+function fazerPergunta() {
+  if (inputPergunta.value == '') {
+    alert('Digite sua pergunta')
+    return
+  }
+  const pergunta = '<div>' + inputPergunta.value + '</div>'
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+  //console.log(respostas[numeroAleatorio])
+  elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+  elementoResposta.style.opacity = 1
+  setTimeout(function () {
+    elementoResposta.style.opacity = 0
+  }, 3000)
+}
